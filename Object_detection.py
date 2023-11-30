1e461583bda444b455257b085c25a3a209f23678
Dorian Donaj Magasic <88779842+Dorijan9@users.noreply.github.com>
2023-11-30 22:38:44 +0000

    Update Object_detection.py

--- Object_detection.py
+++ Object_detection.py
@@ -1,12 +1,11 @@
-# Open Webcam
-# Set PYPLOT environment
-# Takes pictures
-#Import necessary modules
 import cv2
 import matplotlib.pyplot as plt
 import time
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
+
+def setup_webcam():
+    return cv2.VideoCapture(0)
 
 def pyplot_setup():
     plt.close()
@@ -14,46 +13,50 @@ def pyplot_setup():
     fig, ax = plt.subplots()
     return fig, ax
 
-def take_pictures(v, a, c, w) :
-    for g in range(c, w + 1):
-        if g == c:
-            print(f"Taking image number {g}")
-            time.sleep(0.001)
-            r, f = v.read()
-            bbox, label, conf = cv.detect_common_objects(f, confidence=0.1, nms_thresh=0.55, model='yolov3', enable_gpu=False)
-            output_image = draw_bbox(f, bbox, label, conf)
-            im = a.imshow(output_image)
-            plt.pause(0.01)
-            plt.show()
-            plt.imshow(output_image)
-            plt.savefig(f'test-{g}.png')
-        else:
-            print(f"Taking image number {g}")
-            r, f = v.read()
-            time.sleep(0.001)
-            bbox, label, conf = cv.detect_common_objects(f, confidence=0.1, nms_thresh=0.55, model='yolov3', enable_gpu=False)
-            output_image = draw_bbox(f, bbox, label, conf)
-            im.set_data(output_image)
-            fig.canvas.draw_idle()
-            im.autoscale()
-            plt.pause(0.01)
-            plt.show()
-            plt.imshow(output_image)
-            plt.savefig(f'test-{g}.png')
+def capture_and_display(v, a, g):
+    print(f"Taking image number {g}")
+    time.sleep(0.001)
+    _, frame = v.read()
+    bbox, label, conf = cv.detect_common_objects(frame, confidence=0.1, nms_thresh=0.55, model='yolov3', enable_gpu=False)
+    output_image = draw_bbox(frame, bbox, label, conf)
+    im = a.imshow(output_image)
+    plt.pause(0.01)
+    plt.show()
+    plt.imshow(output_image)
+    plt.savefig(f'test-{g}.png')
+    return im
 
+def take_pictures(v, a, start_counter, end_counter):
+    for g in range(start_counter, end_counter + 1):
+        im = capture_and_display(v, a, g) if g == start_counter else update_display(v, im, g)
 
-#Set up variables
-w = cv2.VideoCapture(0)
-fig, ax = pyplot_setup()
-counter = 1
-wish = int(input("Enter how many photos do you want to take: "))
-take_pictures(w, ax, counter, wish)
+def update_display(v, im, g):
+    print(f"Taking image number {g}")
+    _, frame = v.read()
+    time.sleep(0.001)
+    bbox, label, conf = cv.detect_common_objects(frame, confidence=0.1, nms_thresh=0.55, model='yolov3', enable_gpu=False)
+    output_image = draw_bbox(frame, bbox, label, conf)
+    im.set_data(output_image)
+    plt.pause(0.01)
+    plt.show()
+    plt.imshow(output_image)
+    plt.savefig(f'test-{g}.png')
 
-#Create loop if user wants to take more pictures
-d = input("Please enter 'c' if you wish to continue taking photos. If not, press any other letter: ")
-while d == "c":
-    counter = wish + 1
-    wish = int(input("Enter how many more photos do you want to take: ")) + wish
-    take_pictures(w, ax, counter, wish)
-    d = input("Please enter 'c' if you wish to continue taking photos. If not, press any other letter: ")
-w.release()
+def main():
+    webcam = setup_webcam()
+    fig, ax = pyplot_setup()
+    start_counter = 1
+    wish = int(input("Enter how many photos you want to take: "))
+    take_pictures(webcam, ax, start_counter, wish)
+
+    continue_capture = input("Enter 'c' to continue taking photos. Otherwise, press any other key: ")
+    while continue_capture.lower() == "c":
+        start_counter = wish + 1
+        wish = int(input("Enter how many more photos you want to take: ")) + wish
+        take_pictures(webcam, ax, start_counter, wish)
+        continue_capture = input("Enter 'c' to continue taking photos. Otherwise, press any other key: ")
+
+    webcam.release()
+
+if __name__ == "__main__":
+    main()
