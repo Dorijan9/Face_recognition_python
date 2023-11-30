ae9c95cf3a37e782d80519b0e6d8efb4e6b4ffa5
Dorian Donaj Magasic <88779842+Dorijan9@users.noreply.github.com>
2023-11-30 22:33:45 +0000

    Update Face_recognition.py

--- Face_recognition.py
+++ Face_recognition.py
@@ -2,106 +2,80 @@ import face_recognition
 import cv2
 import numpy as np
 
-# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
-# other example, but it includes some basic performance tweaks to make things run a lot faster:
-#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
-#   2. Only detect faces in every other frame of video.
+def load_known_faces():
+    obama_face_encoding = np.load("/home/pi/facerecognition/photos/obama_encoding.npy")
+    biden_face_encoding = np.load("/home/pi/facerecognition/photos/biden_encoding.npy")
+    trump_face_encoding = np.load("/home/pi/facerecognition/photos/trump_encoding.npy")
+    donaj_face_encoding = np.load("/home/pi/facerecognition/photos/donaj_encoding.npy")
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+    known_face_encodings = [
+        obama_face_encoding,
+        biden_face_encoding,
+        trump_face_encoding,
+        donaj_face_encoding
+    ]
+    known_face_names = [
+        "Barack Obama",
+        "Joe Biden",
+        "Donald Trump",
+        "Dorijan Donaj"
+    ]
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+    return known_face_encodings, known_face_names
 
-
-#Load an already recognized sample picture
-obama_face_encoding = np.load("/home/pi/facerecognition/photos/obama_encoding.npy")
-biden_face_encoding = np.load("/home/pi/facerecognition/photos/biden_encoding.npy")
-trump_face_encoding = np.load("/home/pi/facerecognition/photos/trump_encoding.npy")
-donaj_face_encoding = np.load("/home/pi/facerecognition/photos/donaj_encoding.npy")
-
-# Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding,
-    trump_face_encoding,
-    donaj_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "Joe Biden",
-    "Donald Trump",
-    "Dorijan Donaj"
-]
-
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-
-print("Initiating")
-# Grab a single frame of video
-ret, frame = video_capture.read()
-
-for i in range(1, 3):
+def process_frame(frame, known_face_encodings, known_face_names):
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
-
-    # Only process every other frame of video to save time
 
     # Find all the faces and face encodings in the current frame of video
     face_locations = face_recognition.face_locations(rgb_small_frame)
     face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-    for j in range(1, 3):
-        if face_locations and face_encodings:
-            print("Working")
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "Unknown"
+    face_names = []
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
 
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
+        # Or instead, use the known face with the smallest distance to the new face
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
 
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+        face_names.append(name)
 
-                face_names.append(name)
+    return face_locations, face_names
 
-            process_this_frame = not process_this_frame
+def main():
+    video_capture = cv2.VideoCapture(0)
 
+    known_face_encodings, known_face_names = load_known_faces()
 
-            # Display the results
+    process_this_frame = True
+
+    for i in range(1, 3):
+        ret, frame = video_capture.read()
+
+        face_locations, face_names = process_frame(frame, known_face_encodings, known_face_names)
+
+        if face_locations and face_names:
             for (top, right, bottom, left), name in zip(face_locations, face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
                 top *= 4
                 right *= 4
                 bottom *= 4
                 left *= 4
 
-                # Draw a box around the face
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-                # Draw a label with a name below the face
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
                 font = cv2.FONT_HERSHEY_DUPLEX
                 frame = cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-                print("Done")
                 cv2.imwrite("result.jpg", frame)
-        elif not face_locations and not face_encodings and j == 2:
+        elif not face_locations and not face_names and i == 2:
             cv2.imwrite("result.jpg", frame)
 
-# Release handle to the webcam
-video_capture.release()
+    video_capture.release()
+
+if __name__ == "__main__":
+    main()
